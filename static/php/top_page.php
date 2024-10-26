fb23548e56c43f4da91772c2de9909d97822c9bf
Hyejin <hyejin6499@gmail.com>
2024-10-26 17:40:21 +0900

    アプリ名前だけ変更しました

--- static/php/top_page.php
+++ static/php/top_page.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My App</title>
+    <title>留守るちゃん</title>
     <link rel="stylesheet" href="../css/top_page_index.css">
 </head>
 
