47557ec3ea7c5bc68c06779e9ef4f9ed9d6e150d
Sayaka <sayasayamatu1108@icloud.com>
2024-10-27 10:10:06 +0900

    change target_danger_page

--- static/php/target_danger_page.php
+++ static/php/target_danger_page.php
@@ -34,9 +34,10 @@
           <ul class="nav">
           <li class="header-hover-color"><a href="suspicious_page.php">不審者</a></li>
             <li class="header-hover-color"><a href="known_page.php">知人</a></li>
-            <li class="header-hover-color active"><a href="calender_page.php">カレンダー</a></li>
+            <li class="header-hover-color"><a href="calender_page.php">カレンダー</a></li>
             <li class="header-hover-color"><a href="interphone_page.php">インターホン</a></li>
-            <li class="header-hover-color"><a href="danger_target_page.php">危険人物リスト</a></li>          </ul>
+            <li class="header-hover-color active"><a href="target_danger_page.php">危険人物リスト</a></li>
+          </ul>
         </div>
     </header>
     <h1>危険人物リスト</h1>
